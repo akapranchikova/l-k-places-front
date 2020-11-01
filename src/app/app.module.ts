df710a275035fc350e26b6b3dc2a3f9ccb38ba18
Alice Kapranchikova <kapranchikovaal@gmail.com>
2020-11-01 14:45:09 +0300

    Add leaflet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './common/material.module';
 import {MAT_DIALOG_DEFAULT_OPTIONS, MatDialogModule} from '@angular/material/dialog';
+window['L_PREFER_CANVAS'] = true;
 
 @NgModule({
   declarations: [
--- src/app/map/map/map.component.ts
+++ src/app/map/map/map.component.ts
@@ -1,20 +1,46 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import {AddMarkerModalComponent} from './add-marker-modal/add-marker-modal.component';
 import {FormMode} from '../../common/misc/helper';
+import 'leaflet';
 
+const L = window['L'];
+const TOKEN = 'pk.eyJ1IjoiYWthcHJhbmNoaWtvdmEiLCJhIjoiY2tneXRob3lxMG91ODJzb3NlNGt6Z2wxcyJ9.u6IgyPtPQpZhtUQldevAsw';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, AfterViewInit {
 
   FormMode = FormMode;
+  map;
+  @ViewChild('map') mapElement: ElementRef;
+
   constructor(private dialog: MatDialog) {
   }
 
   ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit() {
+    this.mapElement.nativeElement.style.height = `300px`;
+    this.map = L.map(this.mapElement.nativeElement, {
+      center: [51.505, -0.09],
+      zoom: 6,
+      // layers: layers,
+      zoomDelta: 0.25,
+      zoomSnap: 0,
+    });
+    L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${TOKEN}`, {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'your.mapbox.access.token'
+    }).addTo(this.map);
   }
 
   openAddMarkerModal(mode: FormMode, element?) {
